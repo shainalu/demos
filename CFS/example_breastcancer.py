fec7aa6c45063499ae610059bb4b233ae220b56c
Shaina <shainalu@gmail.com>
2022-11-17 22:58:55 -0500

    save featsets

--- CFS/example_breastcancer.py
+++ CFS/example_breastcancer.py
@@ -1,4 +1,8 @@
+"""
+Example running CFS on sklearn breast cancer data
 
+Last Modified: November 2022
+"""
 
 # imports
 import pandas as pd
@@ -8,23 +12,25 @@ from sklearn.model_selection import train_test_split
 from CFS.cfs import zscore, applyCFS
 
 from sklearn.datasets import load_breast_cancer
+
 data = load_breast_cancer()
-df = pd.DataFrame(data['data'])
-target = pd.Series(data['target'])
+df = pd.DataFrame(data["data"])
+target = pd.Series(data["target"])
 
-#train/test split
-Xtrain, Xtest, ytrain, ytest = train_test_split(df, target, test_size=0.2,\
-                                                random_state=9, shuffle=True,\
-                                                stratify=target)
+# train/test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    df, target, test_size=0.2, random_state=9, shuffle=True, stratify=target
+)
 
-#z-score train and test folds
+# z-score train and test folds
 Xtrain = zscore(Xtrain)
 Xtest = zscore(Xtest)
 
-#CFS
-featset, train_auroc, test_auroc = applyCFS(Xtrain, Xtest, ytrain, ytest)
+# CFS
+featset, train_auroc, test_auroc = applyCFS(Xtrain, Xtest, ytrain, ytest, 10, 10)
 
-print("featuresets")
-print(featset)
-print(train_auroc)
-print(test_auroc)
+# outputs
+summary = pd.concat([featset, train_auroc, test_auroc], axis=1)
+summary.columns = ["feature sets", "train AUROC", "test AUROC"]
+print(summary)
+# summary.to_csv("featuresets.csv")
